34478a8e8a11ab00e931fe2badff17af057e8af0
rrockru <rrock.ru@gmail.com>
2013-04-05 03:42:28 +0400

    Не работает не винде. Коммит для переноса ни Линь.

--- src/updater.cpp
+++ src/updater.cpp
@@ -12,20 +12,38 @@ namespace Ui
         _appName = QApplication::applicationName();
         _remoteVersion = QString::fromWCharArray(QGEN_VER);
 
+        QWidget *widget = new QWidget(this);
         QVBoxLayout *vbox = new QVBoxLayout;
-        QPushButton *button = new QPushButton("Launch", this);
-        connect(button, SIGNAL(clicked()), this, SLOT(OnLaunchButton()));
-        vbox->addWidget(button);
 
-        setLayout(vbox);
+        _textEdit = new QTextEdit(this);
+        _textEdit->setReadOnly(true);
+        _fileProgress = new QProgressBar(this);
+        _fileProgress->setTextVisible(false);
+        _totalProgress = new QProgressBar(this);
+        //_totalProgress->setTextVisible(false);
+
+        launchButton = new QPushButton("Launch", this);
+        launchButton->setEnabled(false);
+        connect(launchButton, SIGNAL(clicked()), this, SLOT(OnLaunchButton()));
+
+        vbox->addWidget(_textEdit);
+        vbox->addWidget(_fileProgress);
+        vbox->addWidget(_totalProgress);
+        vbox->addWidget(launchButton);
+
+        widget->setLayout(vbox);
+        setCentralWidget(widget);
+    }
+
+    Updater::~Updater()
+    {
+        delete download;
     }
 
     bool Updater::CheckForUpdate()
     {
         QString url = "http://dl.dropbox.com/u/16929037/QGen/" + _appName + ".ver";
 
-        QNetworkAccessManager manager;
-        QNetworkReply *download;
         QEventLoop loop;
 
         QTimer::singleShot(5 * 1000, &loop, SLOT(quit()));
@@ -57,8 +75,35 @@ namespace Ui
         if(_remoteVersion  != QString::fromWCharArray(QGEN_VER))
         {
             QString desc = qgen.firstChildElement("Desc").text();
+            QDomElement fileToUpdate = qgen.firstChildElement("File");
+            while (!fileToUpdate.isNull()) {
+                QString fileName = fileToUpdate.attribute("name");
+                QString fileSum = fileToUpdate.attribute("sum");
+                if (!QFileInfo(QApplication::applicationDirPath() + QDir::separator() + fileName).exists())
+                {
+                    _filesToUpdate[fileName] = fileToUpdate.attribute("size").toUInt();
+                } else if (fileSum != GetMD5Sum(QApplication::applicationDirPath() + QDir::separator() + fileName))
+                {
+                    _filesToUpdate[fileName] = fileToUpdate.attribute("size").toUInt();
+                }
+                fileToUpdate = fileToUpdate.nextSiblingElement("File");
+            }
+
+            quint64 sizeToDownload = 0;
+
+            QMapIterator<QString, quint64> iter(_filesToUpdate);
+            while (iter.hasNext()) {
+                iter.next();
+                sizeToDownload += iter.value();
+            }
+
+            desc += tr("<br/>Need to download %1").arg(ConvertSize(sizeToDownload));
+
             UpdateShow *dlg  = new UpdateShow(_remoteVersion, desc, this);
-            return dlg->exec();
+            if(dlg->exec())
+            {
+                return true;
+            }
         }
         return false;
     }
@@ -71,7 +116,6 @@ namespace Ui
 #else
         path = _appPath + QDir::separator() + _appName;
 #endif
-        qDebug() << path;
         QProcess::startDetached(path);
         close();
     }
@@ -130,7 +174,10 @@ namespace Ui
         for (int i = 0; i < list.size(); ++i) {
             QDomElement file = doc.createElement("File");
             file.setAttribute("name", directory.relativeFilePath(list.at(i)));
-            file.setAttribute("sum", GetMD5Sum(QApplication::applicationDirPath() + QDir::separator() + list.at(i)));
+            file.setAttribute("sum", GetMD5Sum(list.at(i)));
+
+            QFileInfo fileInfo(list.at(i));
+            file.setAttribute("size", fileInfo.size());
             qgen.appendChild(file);
         }
 
@@ -149,7 +196,7 @@ namespace Ui
     QStringList Updater::GetFileList(const QDir &directory)
     {
         QStringList tmpList;
-        QDirIterator dirIt(directory,QDirIterator::Subdirectories);
+        QDirIterator dirIt(directory, QDirIterator::Subdirectories);
 
         while (dirIt.hasNext()) {
             dirIt.next();
@@ -176,7 +223,6 @@ namespace Ui
             }
             else
                 tmpDir.mkpath(tmpDir.absolutePath());
-            qDebug() << tmpDir.absolutePath() + QDir::separator() + filePath;
             if (!QFile::copy(list.at(i), tmpDir.absolutePath() + QDir::separator() + filePath))
                 return false;
         }
@@ -187,6 +233,15 @@ namespace Ui
         qgen.setAttribute("version", QString::fromWCharArray(QGEN_VER));
         qgen.setAttribute("path", QApplication::applicationDirPath());
         root.appendChild(qgen);
+
+        QMapIterator<QString, quint64> iter(_filesToUpdate);
+        while (iter.hasNext()) {
+            iter.next();
+            QDomElement file = _updateFile.createElement("File");
+            file.setAttribute("name", iter.key());
+            file.setAttribute("size", iter.value());
+            qgen.appendChild(file);
+        }
 
         QFile file(tmpDir.absolutePath() + QDir::separator() + _appName + ".ver");
         if (!file.open(QIODevice::WriteOnly))
@@ -246,24 +301,176 @@ namespace Ui
         file.close();
 
         QDomElement root = _updateFile.documentElement();
-        QDomNode n = root.firstChild();
-        while (!n.isNull()) {
-            if (n.isElement()) {
-                QDomElement e = n.toElement();
-                if (e.attribute("version") == QString::fromWCharArray(QGEN_VER))
-                {
-                    _appPath = e.attribute("path");
-                    break;
-                }
+        QDomElement qgen = root.firstChildElement("QGen");
+        while (!qgen.isNull()) {
+            if (qgen.attribute("version") == QString::fromWCharArray(QGEN_VER))
+            {
+                _appPath = qgen.attribute("path");
+                break;
             }
-            n = n.nextSibling();
+            qgen = qgen.nextSiblingElement("QGen");
         }
+
+        QDomElement fileElement = qgen.firstChildElement("File");
+        while (!fileElement.isNull()) {
+            _filesToUpdate[fileElement.attribute("name")] = fileElement.attribute("size").toUInt();
+            fileElement = fileElement.nextSiblingElement("File");
+        }
+
+        if(_filesToUpdate.isEmpty())
+            return false;
 
         if(_appPath.isEmpty())
             return false;
 
         this->show();
 
-        return true;
+        return StartUpdate();
+    }
+
+    QString Updater::ConvertSize(quint64 size)
+    {
+        const quint64 kb = 1024;
+        const quint64 mb = 1024 * kb;
+        const quint64 gb = 1024 * mb;
+        const quint64 tb = 1024 * gb;
+
+        if (size >= tb)
+            return tr("%1 TB").arg(QLocale().toString(qreal(size) / tb, 'f', 3));
+        if (size >= gb)
+            return tr("%1 GB").arg(QLocale().toString(qreal(size) / gb, 'f', 2));
+        if (size >= mb)
+            return tr("%1 MB").arg(QLocale().toString(qreal(size) / mb, 'f', 1));
+        if (size >= kb)
+            return tr("%1 KB").arg(QLocale().toString(size / kb));
+        return tr("%1 byte(s)").arg(QLocale().toString(size));
+    }
+
+    bool Updater::StartUpdate()
+    {
+        int fileNum = 0;
+        downloadRequestAborted = false;
+
+        _totalProgress->setMaximum(_filesToUpdate.count());
+        _totalProgress->setValue(fileNum);
+        _totalProgress->setFormat(QString("%1/%2").arg(_totalProgress->value()).arg(_filesToUpdate.count()));
+        _downloadPath = QDir::tempPath() + QDir::separator() + _appName + "Upd";
+        QDir tmpDir(_downloadPath);
+        CleanFolder(tmpDir.absolutePath());
+
+        QMapIterator<QString, quint64> iter(_filesToUpdate);
+        while (iter.hasNext()) {
+            iter.next();
+
+            if(downloadRequestAborted)
+                return false;
+
+            QString filePath = iter.key();
+            if(filePath.contains("/"))
+            {
+                QString fileDir = filePath.left(filePath.lastIndexOf("/"));
+                if(!QDir(fileDir).exists())
+                    tmpDir.mkpath(tmpDir.absolutePath() + QDir::separator() + fileDir);
+            }
+            else
+                if(!QDir(tmpDir.absolutePath()).exists())
+                    tmpDir.mkpath(tmpDir.absolutePath());
+
+            filePath = _downloadPath + QDir::separator() + filePath;
+
+            qDebug() << filePath;
+
+            if(QFile::exists(filePath))
+            {
+                QFile::remove(filePath);
+            }
+
+            _downFile = new QFile(filePath);
+            if(!_downFile->open(QIODevice::WriteOnly))
+            {
+                return false;
+            }
+
+            QString url = "http://dl.dropbox.com/u/16929037/QGen/" + iter.key();
+
+            _textEdit->append(tr("Downloading %1 (%2)...<br/>").arg(iter.key(), ConvertSize(iter.value())));
+
+            QEventLoop loop;
+
+            download = manager.get(QNetworkRequest(QUrl(url)));
+            connect(download, SIGNAL(finished()), this, SLOT(OnDownloadFinished()));
+            connect(download, SIGNAL(readyRead()), this, SLOT(OnDownloadReadyRead()));
+            connect(download, SIGNAL(downloadProgress(qint64,qint64)), this, SLOT(OnDownloadProgress(qint64,qint64)));
+            connect(download, SIGNAL(finished()), &loop, SLOT(quit()));
+            loop.exec();
+            _totalProgress->setValue(++fileNum);
+            _totalProgress->setFormat(QString("%1/%2").arg(_totalProgress->value()).arg(_filesToUpdate.count()));
+        }
+        if (!downloadRequestAborted)
+        {
+            QMapIterator<QString, quint64> iter(_filesToUpdate);
+            while (iter.hasNext()) {
+                iter.next();
+                QFile::remove(_appPath + QDir::separator() + iter.key());
+                QFile::copy(_downloadPath + QDir::separator() + iter.key(), _appPath + QDir::separator() + iter.key());
+            }
+            launchButton->setEnabled(true);
+            return true;
+        }
+        return false;
+    }
+
+    void Updater::OnDownloadProgress(qint64 bytesReceived, qint64 bytesTotal)
+    {
+        if(downloadRequestAborted)
+            return;
+        _fileProgress->setMaximum(bytesTotal);
+        _fileProgress->setValue(bytesReceived);
+    }
+
+    void Updater::OnDownloadReadyRead()
+    {
+        if(_downFile)
+            _downFile->write(download->readAll());
+    }
+
+    void Updater::OnDownloadFinished()
+    {
+        if(downloadRequestAborted)
+        {
+            if(_downFile)
+            {
+                _downFile->close();
+                _downFile->remove();
+                delete _downFile;
+                _downFile = NULL;
+            }
+            download->deleteLater();
+            return;
+        }
+
+        OnDownloadReadyRead();
+        _downFile->flush();
+        _downFile->close();
+
+        if(download->error())
+        {
+            _textEdit->append(tr("<font color=\"red\">Failed: %1</font>").arg(download->errorString()));
+            _downFile->remove();
+            downloadRequestAborted = true;
+        }
+
+        download->deleteLater();
+        download = NULL;
+        delete _downFile;
+        _downFile = NULL;
+    }
+
+    void Updater::closeEvent(QCloseEvent *event)
+    {
+        downloadRequestAborted = true;
+        download->abort();
+
+        QMainWindow::closeEvent(event);
     }
 }
--- src/updater.h
+++ src/updater.h
@@ -8,6 +8,9 @@
 #include <QNetworkRequest>
 #include <QNetworkReply>
 #include <QDomDocument>
+#include <QPlainTextEdit>
+#include <QProgressBar>
+#include <QPushButton>
 
 namespace Ui
 {
@@ -17,6 +20,7 @@ namespace Ui
         Q_OBJECT
     public:
         explicit Updater();
+        ~Updater();
 
         bool CheckForUpdate();
         bool GenerateUpdateFile();
@@ -26,23 +30,44 @@ namespace Ui
 
     signals:
 
+    protected:
+        void closeEvent(QCloseEvent * event);
+
     public slots:
 
     private slots:
         void OnLaunchButton();
+        void OnDownloadProgress(qint64,qint64);
+        void OnDownloadFinished();
+        void OnDownloadReadyRead();
+        //void OnCancelDownload();
 
     private:
         bool CleanFolder(const QString &directory);
         QStringList GetFileList(const QDir &directory);
         QString GetMD5Sum(const QString &fileName);
+        QString ConvertSize(quint64);
+        bool StartUpdate();
 
         QString _appName;
         QString _remoteVersion;
         QString _appPath;
+        QString _downloadPath;
 
         QDomDocument _updateFile;
+        QMap<QString, quint64> _filesToUpdate;
+        QTextEdit *_textEdit;
+        QProgressBar *_fileProgress;
+        QProgressBar *_totalProgress;
+        QFile *_downFile;
+
+        QNetworkAccessManager manager;
+        QNetworkReply *download;
+
+        QPushButton *launchButton;
 
         quint32 _crc32table[256];
+        bool downloadRequestAborted;
     };
 } // namespace Ui
 
